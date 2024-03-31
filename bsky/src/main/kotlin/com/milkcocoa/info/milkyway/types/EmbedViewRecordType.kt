8082799c257882da3ac1c7d5047a0b5713f46902
keita <developer@milkcocoa.info>
2024-04-01 00:03:44 +0900

    :recycle: merge enum and its serializer

---
+++ bsky/src/main/kotlin/com/milkcocoa/info/milkyway/types/EmbedViewRecordType.kt
@@ -0,0 +1,17 @@
+package com.milkcocoa.info.milkyway.types
+
+import com.milkcocoa.info.milkyway.util.SerializableEnum
+import kotlinx.serialization.Serializable
+
+@Serializable(with = EmbedViewRecordType.Companion.Serializer::class)
+enum class EmbedViewRecordType(override val identifier: String) : SerializableEnum{
+    ViewRecord("app.bsky.embed.record.viewRecord#viewRecord"),
+    UnknownEmbed("unknown");
+
+    companion object{
+        fun getByIdentifier(identifier: String?) =
+            entries.find { it.identifier == identifier } ?: UnknownEmbed
+        object Serializer: SerializableEnum.Companion.SerializableEnumSerializer<EmbedViewRecordType>(EmbedViewRecordType::class)
+    }
+}
+
