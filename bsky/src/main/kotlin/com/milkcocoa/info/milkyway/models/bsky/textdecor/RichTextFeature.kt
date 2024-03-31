414a732099c95707f61eb4e29e1a908954c9b75f
keita <developer@milkcocoa.info>
2024-04-01 00:07:22 +0900

    :sparkles: add a model for richtext for link

---
+++ bsky/src/main/kotlin/com/milkcocoa/info/milkyway/models/bsky/textdecor/RichTextFeature.kt
@@ -0,0 +1,31 @@
+package com.milkcocoa.info.milkyway.models.bsky.textdecor
+
+import com.milkcocoa.info.milkyway.models.bsky.feed.FeedPostRecord
+import com.milkcocoa.info.milkyway.models.bsky.record.BskyRecord
+import com.milkcocoa.info.milkyway.types.RecordType
+import com.milkcocoa.info.milkyway.types.RichTextType
+import com.milkcocoa.info.milkyway.util.JsonElementUtil.type
+import kotlinx.serialization.DeserializationStrategy
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonContentPolymorphicSerializer
+import kotlinx.serialization.json.JsonElement
+
+@Serializable(with = RichTextFeature.Companion::class)
+abstract class RichTextFeature {
+    @SerialName("\$type")
+    abstract val type: RichTextType
+
+    companion object : JsonContentPolymorphicSerializer<RichTextFeature>(RichTextFeature::class){
+        override fun selectDeserializer(element: JsonElement): DeserializationStrategy<RichTextFeature> {
+            return when(RichTextType.getByIdentifier(element.type)){
+                RichTextType.RichTextTypeLink -> RichTextLink.serializer()
+                else -> Unknown.serializer()
+            }
+        }
+        @Serializable
+        class Unknown : RichTextFeature() {
+            override var type = RichTextType.Unknown
+        }
+    }
+}
