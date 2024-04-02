7322c60fa800eed3ac3d3f578d30b340f8d42308
keita <developer@milkcocoa.info>
2024-04-02 23:27:04 +0900

    :rotating_light: add a test of getPreferences

--- bsky/src/test/kotlin/com/millkcococa/info/milkyway/api/bsky/actor/ActorCategoryTest.kt
+++ bsky/src/test/kotlin/com/millkcococa/info/milkyway/api/bsky/actor/ActorCategoryTest.kt
@@ -3,18 +3,15 @@ package com.millkcococa.info.milkyway.api.bsky.actor
 import com.milkcocoa.info.milkyway.Milkyway
 import com.milkcocoa.info.milkyway.api.atproto.server.CreateSession
 import com.milkcocoa.info.milkyway.api.bsky
+import com.milkcocoa.info.milkyway.api.bsky.actor.GetPreferences
 import com.milkcocoa.info.milkyway.api.bsky.actor.GetProfile
-import com.milkcocoa.info.milkyway.api.bsky.feed.GetTimeLine
 import com.milkcocoa.info.milkyway.domain.Domain
 import kotlinx.coroutines.runBlocking
-import java.time.LocalDateTime
-import java.time.ZoneOffset
-import java.time.format.DateTimeFormatter
 import kotlin.test.Test
 
-class GetProfileTest {
+class ActorCategoryTest {
     @Test
-    fun test(){
+    fun getProfile(){
         runBlocking {
             val session = Milkyway.instance(domain = Domain("https://bsky.social"))
                 .atProtocol()
@@ -33,6 +30,31 @@ class GetProfileTest {
                 .getProfile(request = GetProfile.GetProfileRequest(
                     accessJwt = session.accessJwt,
                     actor = session.didDoc.id
+                ))
+                .let {
+                    println(it)
+                }
+        }
+    }
+
+    @Test
+    fun getPreferences(){
+        runBlocking {
+            val session = Milkyway.instance(domain = Domain("https://bsky.social"))
+                .atProtocol()
+                .server()
+                .createSession(
+                    CreateSession.CreateSessionRequest(
+                        identifier = System.getenv("BSKY_IDENTIFIER"),
+                        password = System.getenv("BSKY_PASSWORD")
+                    )
+                )
+
+            Milkyway.instance(domain = Domain("https://bsky.social"))
+                .bsky()
+                .actor()
+                .getPReferences(request = GetPreferences.GetPreferencesRequest(
+                    accessJwt = session.accessJwt
                 ))
                 .let {
                     println(it)
