8c61a67e224a544b9fdc338a0fc5af6bb84e290b
keita <developer@milkcocoa.info>
2024-04-02 21:58:21 +0900

    :rotating_light: add a test for getProfile

---
+++ bsky/src/test/kotlin/com/millkcococa/info/milkyway/api/bsky/actor/GetProfileTest.kt
@@ -0,0 +1,42 @@
+package com.millkcococa.info.milkyway.api.bsky.actor
+
+import com.milkcocoa.info.milkyway.Milkyway
+import com.milkcocoa.info.milkyway.api.atproto.server.CreateSession
+import com.milkcocoa.info.milkyway.api.bsky
+import com.milkcocoa.info.milkyway.api.bsky.actor.GetProfile
+import com.milkcocoa.info.milkyway.api.bsky.feed.GetTimeLine
+import com.milkcocoa.info.milkyway.domain.Domain
+import kotlinx.coroutines.runBlocking
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
+import kotlin.test.Test
+
+class GetProfileTest {
+    @Test
+    fun test(){
+        runBlocking {
+            val session = Milkyway.instance(domain = Domain("https://bsky.social"))
+                .atProtocol()
+                .server()
+                .createSession(
+                    CreateSession.CreateSessionRequest(
+                        identifier = System.getenv("BSKY_IDENTIFIER"),
+                        password = System.getenv("BSKY_PASSWORD")
+                    )
+                )
+
+
+            Milkyway.instance(domain = Domain("https://bsky.social"))
+                .bsky()
+                .actor()
+                .getProfile(request = GetProfile.GetProfileRequest(
+                    accessJwt = session.accessJwt,
+                    actor = session.didDoc.id
+                ))
+                .let {
+                    println(it)
+                }
+        }
+    }
+}
